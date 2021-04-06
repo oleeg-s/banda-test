5f96a51e05737417fca3e9e0e75d4ccec6671d45
Oleg <example@gmail.com>
2021-04-06 21:18:17 +0300

    test commit

---
+++ src/components/potal.js
@@ -0,0 +1,61 @@
+import React, { useRef, useEffect } from 'react';
+import { createPortal } from 'react-dom';
+
+
+function createRootElement(id) {
+  const rootContainer = document.createElement('div');
+  rootContainer.setAttribute('id', id);
+  return rootContainer;
+}
+
+function addRootElement(rootElem) {
+  document.body.insertBefore(
+    rootElem,
+    document.body.lastElementChild.nextElementSibling,
+  );
+}
+
+function usePortal(id) {
+  const rootElemRef = useRef(null);
+
+  useEffect(function setupElement() {
+    // Look for existing target dom element to append to
+    const existingParent = document.querySelector(`#${id}`);
+    // Parent is either a new root or the existing dom element
+    const parentElem = existingParent || createRootElement(id);
+
+    // If there is no existing DOM element, add a new one.
+    if (!existingParent) {
+      addRootElement(parentElem);
+    }
+
+    // Add the detached element to the parent
+    parentElem.appendChild(rootElemRef.current);
+
+    return function removeElement() {
+      rootElemRef.current.remove();
+      if (!parentElem.childElementCount) {
+        parentElem.remove();
+      }
+    };
+  }, [id]);
+
+  function getRootElem() {
+    if (!rootElemRef.current) {
+      rootElemRef.current = document.createElement('div');
+    }
+    return rootElemRef.current;
+  }
+
+  return getRootElem();
+}
+
+const Portal = ({ id, children, count }) => {
+    const target = usePortal(id);
+    return createPortal(
+      children,
+      target,
+    );
+  };
+  
+  export default Portal;
